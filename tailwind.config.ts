b74474a992d6486aab1a18ef15efe35cf23b022b
Katja Hollaar <afkatja@gmail.com>
2024-12-01 11:20:11 -0600

    tailwindconfig as a module

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const tailwindConfig = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [require("@tailwindcss/typography")],
 }
+
+export default tailwindConfig
