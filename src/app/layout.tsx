679d121eb3ad9f7fca3c38de0bfac6d6035f4a46
Katja Hollaar <afkatja@gmail.com>
2024-12-01 20:50:31 -0600

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,9 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "Digital resume of web developer Katja Hollaar",
   description: "My portfolio of web projects",
+  icons: {
+    icon: "/favicon.ico",
+  },
 }
 
 export default function RootLayout({
