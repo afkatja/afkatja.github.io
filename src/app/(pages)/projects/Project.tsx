625d9040bce43404131b641b198b93e985300483
Katja Hollaar <afkatja@gmail.com>
2025-04-30 13:55:52 -0600

    add FG and update layout for projects

--- src/app/(pages)/projects/Project.tsx
+++ src/app/(pages)/projects/Project.tsx
@@ -24,48 +24,55 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
   return (
     <div
       className={`bg-neutral-200 dark:bg-neutral-800 text-neutral-900 dark:text-neutral-100 project-card rounded-lg shadow-lg p-6 my-5 mx-0 md:m-0 ${
-        highlight ? "col-span-4" : ""
+        highlight
+          ? "md:col-span-4 md:grid md:grid-cols-2 md:gap-4"
+          : "col-span-2"
       }`}
     >
       {imageUrl && (
         <Image
           src={imageUrl}
           alt={`${title} project screenshot`}
-          width={400}
-          height={250}
-          className="rounded-t-lg"
+          width={highlight ? 400 : 250}
+          height={highlight ? 250 : 100}
+          className="rounded-t-lg mt-0"
         />
       )}
-      <h3 className="text-xl font-bold mt-4">{title}</h3>
-      <p className="mt-2">{description}</p>
-      <div className="technologies mt-4">
-        {technologies.map(tech => (
-          <span
-            key={tech}
-            className="inline-block bg-neutral-200 dark:bg-neutral-400 text-neutral-700 dark:text-neutral-100 rounded-full px-3 py-1 text-sm mr-2 mb-2"
-          >
-            {tech}
-          </span>
-        ))}
-      </div>
-      <div className="links mt-4 flex space-x-4">
-        {githubLink && (
-          <Link
-            href={githubLink}
-            target="_blank"
-            className="text-blue-600 dark:text-blue-100 hover:underline"
-          >
-            GitHub
-          </Link>
-        )}
-        {liveLink && (
-          <Link
-            href={liveLink}
-            target="_blank"
-            className="text-blue-600 dark:text-blue-100 no-underline hover:underline"
-          >
-            Live Site
-          </Link>
+      <div className="flex flex-col h-[calc(100%-100px)]">
+        <h3 className="text-xl font-bold mt-4">{title}</h3>
+        <p className="mt-2">{description}</p>
+        <div className="technologies my-4">
+          <h4 className="font-semibold">Technologies:</h4>
+          {technologies.map(tech => (
+            <span
+              key={tech}
+              className="inline-block bg-neutral-400 dark:bg-neutral-100 text-neutral-700 dark:text-neutral-100 rounded-full px-3 py-1 text-sm mr-2 mb-2"
+            >
+              {tech}
+            </span>
+          ))}
+        </div>
+        {(githubLink || liveLink) && (
+          <div className="links flex space-x-4 text-sm mt-auto">
+            {githubLink && (
+              <Link
+                href={githubLink}
+                target="_blank"
+                className="text-blue-600 dark:text-blue-100 no-underline hover:underline"
+              >
+                GitHub
+              </Link>
+            )}
+            {liveLink && (
+              <Link
+                href={liveLink}
+                target="_blank"
+                className="text-blue-600 dark:text-blue-100 no-underline hover:underline"
+              >
+                Live Site
+              </Link>
+            )}
+          </div>
         )}
       </div>
     </div>
--- src/app/(pages)/projects/page.tsx
+++ src/app/(pages)/projects/page.tsx
@@ -3,21 +3,31 @@ import ProjectCard, { ProjectCardProps } from "./Project"
 
 const projects: ProjectCardProps[] = [
   {
+    title: "Finca Guarumo",
+    description:
+      "A web application for a homestead farm in Costa Rica, built with React, Next.js and a headless CMS Sanity.io. The multilingual and responsive website features a blog and a booking system for an accommodation.",
+    technologies: ["React", "Next.js", "Tailwindcss", "Sanity.io"],
+    imageUrl: "/fincaguarumo.jpg",
+    githubLink: "https://github.com/afkatja/fincaguarumo",
+    liveLink: "http://fincaguarumo.com",
+    highlight: true,
+  },
+  {
     title: "Greenwheels",
     description:
-      "A comprehensive web application revolutionizing urban mobility through intuitive, cross-platform car sharing solutions. Migrated from fragmented, multi-framework spaghetti code to a robust, scalable React ecosystem. Developed a multiplatform React application ensuring seamless user experience. Created a custom component library, reducing development overhead and maintaining UI/UX consistency. Integrated Google Maps Platform to enhance car findability and booking experience. Implemented cross-platform strategy enabling smooth iOS and Android app integration.Significantly improved application performance, scalability, and maintainability. Simplified urban mobility by creating an intuitive, reliable car-sharing platform that works across web and mobile environments.",
+      "A comprehensive web application revolutionizing urban mobility through intuitive, cross-platform car sharing solutions. Migrated from fragmented, multi-framework code to a robust, scalable React ecosystem. Developed a multiplatform React application ensuring seamless user experience. Created a custom component library, reducing development overhead and maintaining UI/UX consistency. Integrated Google Maps Platform to enhance car findability and booking experience. Implemented cross-platform strategy enabling smooth iOS and Android app integration. Significantly improved application performance, scalability, and maintainability.",
     technologies: ["React", "next.js", "styled components", "GraphQL", "node"],
     imageUrl: "greenwheels.png",
     githubLink: "",
     liveLink: "",
-    highlight: true,
+    highlight: false,
   },
   {
     title: "Bolster",
     description:
       "Front-end development of a flexible and versatile system of custom built components and templates to empower NGOs and independent journalistic organizations on the web. Tailoring the UI for the specific clients' needs and requirements. Constantly improving and further developing responsive and browser/OS agnostic design system.",
     technologies: ["React", "wordpress", "styled components", "node"],
-    imageUrl: "",
+    imageUrl: "/bolster.png",
     githubLink: "",
     liveLink: "",
     highlight: false,
@@ -27,7 +37,7 @@ const projects: ProjectCardProps[] = [
     description:
       "The portal website of the Dutch cyclists union, with subsites for local departments. Built with Worpress custom theme platform (Perikles), with advanced custom components and templates, using webpack, sass and ecmascript.",
     technologies: ["React", "wordpress", "sass", "node"],
-    imageUrl: "",
+    imageUrl: "/fietsersbond.svg",
     githubLink: "",
     liveLink: "",
     highlight: false,
@@ -37,7 +47,7 @@ const projects: ProjectCardProps[] = [
     description:
       "The platform for the Dutch digital magazine dealing with the issues of politics, sustainability and citizen awareness.",
     technologies: ["React", "wordpress", "sass", "node"],
-    imageUrl: "",
+    imageUrl: "/oneworld.svg",
     githubLink: "",
     liveLink: "",
     highlight: false,
@@ -47,8 +57,8 @@ const projects: ProjectCardProps[] = [
     description:
       "The learning platform that combines enriched eBooks with social & collaborative learning tools. A web application with a shared interface for schoolchildren, teachers and book editors that enables entirely digital learning environment: reading, writing, making notes and drawings, reviewing homework, playing games like wordweb and enriching book content with interactive elements (video, photo, game, etc).",
     technologies: ["vue", "sass", "node"],
-    liveLink: "schooltas.avif",
-    imageUrl: "",
+    liveLink: "",
+    imageUrl: "/schooltas.avif",
     githubLink: "",
     highlight: false,
   },
@@ -56,7 +66,7 @@ const projects: ProjectCardProps[] = [
     title: "Staatsloterij",
     description:
       "The website of the Dutch State lottery. Responsive webshop and website.",
-    technologies: [".net", "angular", "backbone.js", "sass", "vue"],
+    technologies: [".net", "angular", "backbone.js", "sass"],
     imageUrl: "staatsloterij.avif",
     githubLink: "",
     liveLink: "http://staatsloterij.nl",
