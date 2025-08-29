be9f673ccdfa0527726f4011a517bd4989d80b82
Katja Hollaar <afkatja@gmail.com>
2025-08-29 11:08:09 -0600

    fix: remove output export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  // output: "export",
   basePath: process.env.NODE_ENV === 'production' ? "/afkatja.github.io" : '',
   images: {unoptimized:true}
 };
