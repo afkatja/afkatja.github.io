b5f788f227802ebb5e0fc18edf682045d03cee4a
Katja Hollaar <afkatja@gmail.com>
2025-08-31 13:57:51 -0600

    fix: remove basePath for gh-pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   ...(process.env.NODE_ENV === 'production' && { output: "export"}),
-  ...(process.env.NODE_ENV === 'production' && {basePath:  "/afkatja.github.io"}),
   images: {unoptimized:true}
 };
 
